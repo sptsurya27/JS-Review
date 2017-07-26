781ace34c62267c3ec3784172458b25a85490c5b
Suryameet Singh <sptsurya27@gmail.com>
2017-07-26 19:31:31 -0400

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,83 @@
+//Variables
+var firstName;
+firstName = "Suryameet"; 
+var lastName;
+lastName = "Singh";
+var age;
+age = 23;
+
+
+//String Concatenation
+var fullName;
+fullName = lastName + firstName;
+console.log(fullName);
+
+//Expressions & Operators
+var num1 = 10;
+var num2 = 5;
+numResult = num1 + num2;
+numResult = num1 - num2;
+numResult = num1 * num2;
+numResult = num1 % num2;
+numResult = num1 / num2;
+
+//Linking JavaScript to HTML File
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+</body>
+</html>
+
+//Operators, Boolean Logic, and Conditionals
+
+if(4 = 4) {
+    console.log("It's true");
+} else {
+    console.log("It's False");
+}
+
+if(5 > 2) {
+    console.log("It's more");
+} else {
+    console.log("It's less!");
+}
+
+//Arrays
+var menu = ["Home", "Portfolio", "About", "Contact"];
+
+//Loops
+var menu = ["Home", "Portfolio", "About", "Contact"];
+for (var i in menu) {
+  console.log(menu[i]);
+
+//Switch Statements
+switch (mood().gethungry()) {
+    case happy:
+        text = "I'm feeling great!";
+        break; 
+    case sad:
+        text = "I'm feeling sad!";
+        break;
+    case tired:
+        text = "I'm feeling sleepy!";
+
+//Functions
+function myFunction(firstName,lastName)
+function 
+
+//Objects
+var person(fullName, age, gender, location );
+
+var res = fullName.toUpperCase();
+
+//DOM
+The Document Object Model is a programming API for documents
+
+//Git and Github
+
+
+
+
